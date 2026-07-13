3ad6691ccccf7bc77253da8fad52dc3342df3745
Sarang Chaudhary <asteeve125@gmail.com>
2026-07-13 16:18:15 +0530

    chore: configure server for Render deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
